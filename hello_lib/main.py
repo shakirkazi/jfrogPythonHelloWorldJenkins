21e4b9e292a28beddb41e81a59dea5594b1257b4
shakirkazi <shakirkazi@gmail.com>
2024-07-24 19:04:13 +0530

    Hello World python hello_lib

---
+++ hello_lib/main.py
@@ -0,0 +1,12 @@
+def hello(user):
+    """
+    Greets the user.
+
+    Args:
+        user (str): The name of the user.
+
+    Returns:
+        str: A greeting message.
+    """
+    greet = " Jfrog python library! v0.6"  # This is Jfrog python library!  print(greet, user)
+    return greet + " " + user
