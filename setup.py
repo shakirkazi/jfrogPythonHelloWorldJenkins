6945fee27c70b9a933f016f0ee566ff42b36c8c4
shakirkazi <shakirkazi@gmail.com>
2024-07-25 14:05:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hello_world',
-    version='0.6',
+    version='0.8',
     packages=find_packages(include=['hello_lib', 'hello_lib.*']),
     install_requires=[],
     entry_points={
