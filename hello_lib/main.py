ab0d45688ddbec511a86640e9bc9587f0bda91c7
shakirkazi <shakirkazi@gmail.com>
2024-07-26 16:31:27 +0530

    Update main.py

--- hello_lib/main.py
+++ hello_lib/main.py
@@ -8,5 +8,5 @@ def hello(user):
     Returns:
         str: A greeting message.
     """
-    greet = " Jfrog python library! v0.8.1"  # This is Jfrog python library!  print(greet, user)
+    greet = " Jfrog python library! v0.9"  # This is Jfrog python library!  print(greet, user)
     return greet + " " + user
